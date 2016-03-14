1d59562c1e96739318dabecbbddeeb36b2b5fcaf
amberhanson <hanson.amber.m@gmail.com>
2016-03-14 18:36:04 -0500

    Update template-tags.php

--- gould/inc/template-tags.php
+++ gould/inc/template-tags.php
@@ -4,9 +4,8 @@
  *
  * Eventually, some of the functionality here could be replaced by core features.
  *
- * @package gould
+ * @package Gould
  */
-
 if ( ! function_exists( 'gould_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
@@ -16,44 +15,76 @@ function gould_posted_on() {
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 	}
-
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
 		esc_attr( get_the_modified_date( 'c' ) ),
 		esc_html( get_the_modified_date() )
 	);
-
 	$posted_on = sprintf(
-		esc_html_x( 'Published %s', 'post date', 'gould' ),
+		esc_html_x( 'published %s', 'post date', 'gould' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
-
 	$byline = sprintf(
 		esc_html_x( 'by %s', 'post author', 'gould' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
-        
-        // Display author avitar if author has a Gravatar
-        
-        $author_id = get_the_author_meta ( 'ID' );
-        if (gould_validate_gravatar( $author_id ) ) {
-            echo '<div class="meta-class has-avatar">';
-            echo '<div class="author-avatar">' . get_avatar ( $author_id ) . '</div>';
-        } else {
-            echo '<div class="meta-content">';
-        }
-        
-	echo '</span><span class="byline"> ' . $byline . '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
-        if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+	
+	// Display the author avatar if the author has a Gravatar
+	$author_id = get_the_author_meta( 'ID' );
+	if ( gould_validate_gravatar( $author_id ) ) {
+		echo '<div class="meta-content has-avatar">';
+		echo '<div class="author-avatar">' . get_avatar( $author_id ) . '</div>';
+	} else {
+		echo '<div class="meta-content">';
+	}
+	
+	echo '<span class="byline"> ' . $byline . '</span><span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+	if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link">';
 		comments_popup_link( esc_html__( 'Leave a comment', 'gould' ), esc_html__( '1 Comment', 'gould' ), esc_html__( '% Comments', 'gould' ) );
 		echo '</span>';
 	}
-        echo '</div><!-- meta content -->';
+	echo '</div><!-- .meta-content -->';
 }
 endif;
-
+if ( ! function_exists( 'gould_index_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for post-date/time and author on index pages.
+ */
+function gould_index_posted_on() {
+	
+	$author_id = get_the_author_meta( 'ID' );
+	
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_attr( get_the_modified_date( 'c' ) ),
+		esc_html( get_the_modified_date() )
+	);
+	$posted_on = sprintf(
+		esc_html_x( 'Published %s', 'post date', 'gould' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+	$byline = sprintf(
+		esc_html_x( 'by %s', 'post author', 'gould' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	);
+	
+	echo '<div class="meta-content">';
+	echo '<span class="byline">' . $byline . ' </span><span class="posted-on">' . $posted_on . ' </span>'; // WPCS: XSS OK.
+	if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( esc_html__( 'Leave a comment', 'gould' ), esc_html__( '1 Comment', 'gould' ), esc_html__( '% Comments', 'gould' ) );
+		echo '</span>';
+	}
+	echo '</div><!-- .meta-content -->';
+}
+endif;
 if ( ! function_exists( 'gould_entry_footer' ) ) :
 /**
  * Prints HTML with meta information for the categories, tags and comments.
@@ -66,20 +97,12 @@ function gould_entry_footer() {
 		if ( $categories_list && gould_categorized_blog() ) {
 			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'gould' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
-
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'gould' ) );
 		if ( $tags_list ) {
 			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'gould' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
 	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link( esc_html__( 'Leave a comment', 'gould' ), esc_html__( '1 Comment', 'gould' ), esc_html__( '% Comments', 'gould' ) );
-		echo '</span>';
-	}
-
 	edit_post_link(
 		sprintf(
 			/* translators: %s: Name of current post */
@@ -91,7 +114,6 @@ function gould_entry_footer() {
 	);
 }
 endif;
-
 /**
  * Returns true if a blog has more than 1 category.
  *
@@ -106,13 +128,10 @@ function gould_categorized_blog() {
 			// We only need to know if there is more than one category.
 			'number'     => 2,
 		) );
-
 		// Count the number of categories that are attached to the posts.
 		$all_the_cool_cats = count( $all_the_cool_cats );
-
 		set_transient( 'gould_categories', $all_the_cool_cats );
 	}
-
 	if ( $all_the_cool_cats > 1 ) {
 		// This blog has more than 1 category so gould_categorized_blog should return true.
 		return true;
@@ -121,7 +140,6 @@ function gould_categorized_blog() {
 		return false;
 	}
 }
-
 /**
  * Flush out the transients used in gould_categorized_blog.
  */
@@ -134,9 +152,6 @@ function gould_category_transient_flusher() {
 }
 add_action( 'edit_category', 'gould_category_transient_flusher' );
 add_action( 'save_post',     'gould_category_transient_flusher' );
-
-
-
 /**
  * Utility function to check if a gravatar exists for a given email or id
  * @param int|string|object $id_or_email A user ID,  email address, or comment object
@@ -185,3 +200,10 @@ function gould_validate_gravatar($id_or_email) {
 		return false;
 	}
 }
+/**
+ * Customize the excerpt read-more indicator
+ */
+function gould_excerpt_more( $more ) {
+	return " …";
+}
+add_filter( 'excerpt_more', 'gould_excerpt_more' );
