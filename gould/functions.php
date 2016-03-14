26d98ac7a3104c293ac93c1c3113258fd77444c3
amberhanson <hanson.amber.m@gmail.com>
2016-03-14 18:41:49 -0500

    Update functions.php

--- gould/functions.php
+++ gould/functions.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * gould functions and definitions.
+ * Gould functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package gould
+ * @package Gould
  */
-
 if ( ! function_exists( 'gould_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
@@ -23,10 +22,8 @@ function gould_setup() {
 	 * to change 'gould' to the name of your theme in all the template files.
 	 */
 	load_theme_textdomain( 'gould', get_template_directory() . '/languages' );
-
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
-
 	/*
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
@@ -34,21 +31,17 @@ function gould_setup() {
 	 * provide it for us.
 	 */
 	add_theme_support( 'title-tag' );
-
 	/*
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
-        set_post_thumbnail_size( 800, 400, true );
-        add_image_size('gould-small-thumb', 300, 150, true);
-
+	set_post_thumbnail_size( 828, 360, true );
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'gould' ),
+		'primary' => esc_html__( 'Primary Menu', 'gould' ),
 	) );
-
 	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
@@ -60,7 +53,6 @@ function gould_setup() {
 		'gallery',
 		'caption',
 	) );
-
 	/*
 	 * Enable support for Post Formats.
 	 * See https://developer.wordpress.org/themes/functionality/post-formats/
@@ -71,18 +63,15 @@ function gould_setup() {
 		'video',
 		'quote',
 		'link',
-                'caption',
 	) );
-
 	// Set up the WordPress core custom background feature.
-//	add_theme_support( 'custom-background', apply_filters( 'gould_custom_background_args', array(
-//		'default-color' => 'ffffff',
-//		'default-image' => '',
-//	) ) );
+	add_theme_support( 'custom-background', apply_filters( 'gould_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
 }
-endif;
+endif; // gould_setup
 add_action( 'after_setup_theme', 'gould_setup' );
-
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -94,7 +83,6 @@ function gould_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'gould_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'gould_content_width', 0 );
-
 /**
  * Register widget area.
  *
@@ -105,19 +93,20 @@ function gould_widgets_init() {
 		'name'          => esc_html__( 'Sidebar', 'gould' ),
 		'id'            => 'sidebar-1',
 		'description'   => '',
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
 }
 add_action( 'widgets_init', 'gould_widgets_init' );
-
 /**
  * Enqueue scripts and styles.
  */
 function gould_scripts() {
 	wp_enqueue_style( 'gould-style', get_stylesheet_uri() );
+	
+wp_enqueue_style( 'gould-style', get_stylesheet_uri() );
         
         wp_enqueue_style('gould-content-sidebar', get_template_directory_uri() . '/layouts/content-sidebar.css' );
         
@@ -142,27 +131,22 @@ function gould_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'gould_scripts' );
-
 /**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
-
 /**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
-
 /**
  * Custom functions that act independently of the theme templates.
  */
 require get_template_directory() . '/inc/extras.php';
-
 /**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
 /**
  * Load Jetpack compatibility file.
  */
